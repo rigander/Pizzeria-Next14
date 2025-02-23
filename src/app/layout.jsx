50874d6c0b6b4c148259668ab40475dac887a4c0
Andrey Komov <andrewkomov@gmail.com>
2025-02-24 00:34:16 +0200

    started header footer components, installed normalize.css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,16 @@
 import '../styles/global.css'
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 function RootLayout({ children }) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>
+        <div className='main-wrapper'>
+            <Header/>
+            {children}
+            <Footer/>
+        </div>
+        </body>
         </html>
     )
 }
