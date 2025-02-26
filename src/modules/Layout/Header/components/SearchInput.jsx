3fd604f7086b39b167886af3776a88f84dd19a65
Andrey Komov <andrewkomov@gmail.com>
2025-02-27 01:42:32 +0200

    Search input update

--- src/modules/Layout/Header/components/SearchInput.jsx
+++ src/modules/Layout/Header/components/SearchInput.jsx
@@ -2,15 +2,24 @@
 
 import React, {useState} from "react";
 import Input from "@/modules/UI/Input";
+import { Search } from 'lucide-react';
 import styles from './SearchInput.module.css';
 
 function SearchInput() {
-
-
+    const [query, setQuery] = useState("");
 
     return(
-        <div className={styles.wrapper}>
-            <Input/>
+        <div className={styles.searchWrapper}>
+            <div className={styles.inputWrapper}>
+                <Search className={styles.searchIcon} size={16} />
+                <Input
+                    className={styles.searchInput}
+                    type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Поиск пиццы..."
+                />
+            </div>
         </div>
     )
 }
