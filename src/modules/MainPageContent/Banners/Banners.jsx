421b56f0566161e5aaeba75107c11ebf4077e604
Andrey Komov <andrewkomov@gmail.com>
2025-03-13 23:06:19 +0200

    work over images

--- src/modules/MainPageContent/Banners/Banners.jsx
+++ src/modules/MainPageContent/Banners/Banners.jsx
@@ -1,35 +1,46 @@
 "use client";
 
-import styles from './Banners.module.scss'
-import {Swiper, SwiperSlide} from "swiper/react";
+import { useState, useEffect } from "react";
+import styles from "./Banners.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Image from "next/image";
-import {banners} from '@/modules/MainPageContent/Banners/lib/images'
+import { banners } from "@/modules/MainPageContent/Banners/lib/images";
 
+export default function Banners() {
+    const [isMounted, setIsMounted] = useState(false);
 
-export default function Banners () {
-    return(
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    return (
         <div className={styles.banners}>
             <Swiper
                 modules={[Pagination, Navigation, Autoplay]}
-                spaceBetween={20}
-                slidesPerView={2}
-                pagination={{ type: 'fraction' }}
+                spaceBetween={15}
+                slidesPerView={3}
+                pagination={{ type: "fraction" }}
                 autoplay={{ delay: 3000, disableOnInteraction: false }}
                 loop={true}
-                style={{ width: "100%", height: "250px" }}
+                style={{ width: "100%", height: "240px" }}
             >
                 {banners.map((image, index) => (
                     <SwiperSlide key={index}>
-                        <Image width={250} height={250}
-                               src={image.src} alt={image.alt} />
+                            <Image
+                            width={600}
+                            height={240}
+                            src={image.src}
+                            alt={image.alt}
+                            priority={isMounted && index === 0}
+                            loading={isMounted && index === 0 ? "eager" : "lazy"}
+                            />
                     </SwiperSlide>
-                    )
-                )}
+                ))}
             </Swiper>
         </div>
-    )
+    );
 }
--- src/modules/MainPageContent/Banners/lib/images.js
+++ src/modules/MainPageContent/Banners/lib/images.js
@@ -1,7 +1,9 @@
 export const banners = [
-    { src: "public/assets/images/banners/first.jpg", alt: "First" },
-    { src: "public/assets/images/banners/second.jpg", alt: "Second" },
-    { src: "public/assets/images/banners/third.jpg", alt: "Third" },
-    { src: "public/assets/images/banners/fourth.jpg", alt: "Fourth" },
-    { src: "public/assets/images/banners/fifth.jpg", alt: "Fifth" },
+    { src: "/assets/images/banners/first.jpg", alt: "First" },
+    { src: "/assets/images/banners/second.jpg", alt: "Second" },
+    { src: "/assets/images/banners/third.jpg", alt: "Third" },
+    { src: "/assets/images/banners/fourth.jpg", alt: "Fourth" },
+    { src: "/assets/images/banners/fifth.jpg", alt: "Fifth" },
+    { src: "/assets/images/banners/sixth.jpg", alt: "Sixth" },
+    { src: "/assets/images/banners/seventh.jpg", alt: "Seventh" },
 ];
